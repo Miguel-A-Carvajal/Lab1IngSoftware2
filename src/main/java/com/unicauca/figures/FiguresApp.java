ce0b150b177973461d99398214024fbe171922c2
JeisonVallejo-MiguelCarvajal <ACER@DESKTOP-B9F5REC>
2024-02-11 20:50:05 -0500

    Commit

---
+++ src/main/java/com/unicauca/figures/FiguresApp.java
@@ -0,0 +1,17 @@
+
+package com.unicauca.figures;
+
+import com.unicauca.figures.domain.presentation.GUIMenu;
+
+/**
+ *
+ * @author libardo
+ */
+public class FiguresApp {
+
+    public static void main(String[] args) {
+        
+        GUIMenu objGuiMen = new GUIMenu();
+        objGuiMen.setVisible(true);
+    }
+}
